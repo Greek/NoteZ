2d97bd770835b73a7eb9510567ded6b11a132bc4
Andreas <a@yeat.dev>
2022-06-13 20:57:23 -0400

    chore: brand change
    Signed-off-by: Andreas <a@yeat.dev>

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function MainLayout({ children }: Props) {
   return (
     <>
-      <Navbar brandName={"NoteZ"} />
+      <Navbar brandName={"Notez"} />
       <main>{children}</main>
     </>
   )
