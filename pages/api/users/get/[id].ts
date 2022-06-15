6810af243fa1716e6705d5fc59336a5fd3c36747
Andreas <a@yeat.dev>
2022-06-15 00:15:51 -0400

    api: new get user endpoint
    Signed-off-by: Andreas <a@yeat.dev>

--- pages/api/users/get/[id].ts
+++ pages/api/users/get/[id].ts
@@ -5,12 +5,13 @@ export default async function getUser(req: NextRequest, res: NextResponse) {
   // @ts-ignore
   const id = req.query.id
 
-  handleGET(id, res)
+  return handleGET(id, res)
 }
 
 async function handleGET(userId: Number, res: any) {
   const user = await prisma.user.findUnique({
     where: { id: Number(userId) },
+    select: { id: true, given_name: true, notes: true }
   })
-  res.json(user)
+  return res.json(user)
 }
---
+++ prisma/migrations/20220615041516_optional_name/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "given_name" DROP NOT NULL;
