194be8f71ed3d397bdebdf40a75ae1db69f6c6ca
Andreas <a@yeat.dev>
2022-06-30 19:29:05 -0400

    chore: export interfaces
    Signed-off-by: Andreas <a@yeat.dev>

--- lib/NoteZ.ts
+++ lib/NoteZ.ts
@@ -1,11 +1,11 @@
-interface Note {
+export interface Note {
   id: Number
   creator_id: Number
   title: string
   content: string
 }
 
-interface UserData {
+export interface UserData {
   id: Number
   email?: string
   name?: string
