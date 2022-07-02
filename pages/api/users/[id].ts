85ae8d699439dad680efe31d2b88ef93f5dd9b8f
Andreas <me@apap04.com>
2022-07-02 02:02:57 -0400

    chore: NextRequest -> NextApiRequest etc
    Signed-off-by: Andreas <me@apap04.com>

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -1,22 +1,20 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextApiRequest, NextApiResponse } from "next"
 import prisma from "../../../lib/prisma"
 
-export default async function getUser(req: NextRequest, res: NextResponse) {
+export default async function getUser(req: NextApiRequest, res: NextApiResponse) {
   // @ts-ignore
   const id = req.query.id
 
   return handleGET(id, res)
 }
 
-async function handleGET(userId: Number, res: NextResponse) {
+async function handleGET(userId: Number, res: NextApiResponse) {
   const user = await prisma.user.findUnique({
     where: { id: Number(userId) },
     select: { id: true, given_name: true, notes: true },
   })
 
-  // @ts-ignore
-  if (!user) return res.json({ error: "User not found" })
+  if (!user) return res.status(404).json({ error: "User not found" })
 
-  // @ts-ignore
-  return res.json(user)
+  return res.status(200).json(user)
 }
