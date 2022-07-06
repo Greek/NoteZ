b1ef3537833cc9e590d6c9f08c7080fbe4639ba8
Andreas <me@apap04.com>
2022-07-05 21:25:39 -0400

    chore: refactor /api/notes/[id]
    Signed-off-by: Andreas <me@apap04.com>

--- pages/api/notes/[id].ts
+++ pages/api/notes/[id].ts
@@ -1,21 +1,42 @@
 import type { NextApiRequest, NextApiResponse } from "next"
+import { getToken } from "next-auth/jwt"
+
 import prisma from "../../../lib/prisma"
 
-export default async function getUser(
+export default async function getNote(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // @ts-ignore
-  const id = req.query.id
+  const { method } = req
 
-  return handleGET(Number(id), res)
+  switch (method) {
+    case "GET":
+      return handleGET(req, res)
+    default:
+      return res.status(405).json({ error: "Only GET methods are allowed." })
+  }
 }
 
-async function handleGET(userId: Number, res: NextApiResponse) {
+async function handleGET(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.body
+  const headers = req.headers
+
+  if (!headers["authorization"]) 
+    return res
+      .status(401)
+      .json({ error: "An authorization token is required." })
+
+  const secret = process.env.NEXTAUTH_SECRET
+  const token = await getToken({ req, secret })
+
+  if (!token) return res.status(401).json({ error: "This token is invalid." })
+
   const note = await prisma.note.findUnique({
-    where: { id: Number(userId) },
+    where: { id: id },
     select: { content: true },
   })
+
+  // if note.
 
   if (!note) return res.status(404).json({ error: "Note not found." })
 
