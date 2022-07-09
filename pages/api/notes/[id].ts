2aa42e540cb19b08eb15ca1fcb51ce7865f3dd43
Andreas <me@apap04.com>
2022-07-09 13:23:15 -0400

    feat(api/notes): add DELETE method
    Signed-off-by: Andreas <me@apap04.com>

--- pages/api/notes/[id].ts
+++ pages/api/notes/[id].ts
@@ -4,6 +4,11 @@ import { json } from "stream/consumers"
 
 import prisma from "../../../lib/prisma"
 
+const TOKEN_INVALID = "Please provide a valid token."
+const ID_INVALID = "Please provide a valid ID."
+const NOTE_NOT_FOUND = "Note not found."
+const NOTE_NOT_YOURS = "This note does not belong to you."
+
 export default async function getNote(
   req: NextApiRequest,
   res: NextApiResponse
@@ -15,8 +20,12 @@ export default async function getNote(
       return handleGET(req, res)
     case "PATCH":
       return handlePATCH(req, res)
+    case "DELETE":
+      return handleDELETE(req, res)
     default:
-      return res.status(405).json({ error: "Only GET methods are allowed." })
+      return res
+        .status(405)
+        .json({ error: "Only GET, PATCH and DELETE methods are allowed." })
   }
 }
 
@@ -27,10 +36,7 @@ async function handleGET(req: NextApiRequest, res: NextApiResponse) {
   const secret = process.env.NEXTAUTH_SECRET
   const token = await getToken({ req, secret })
 
-  if (!token)
-    return res
-      .status(401)
-      .json({ error: "A token is required to make this request." })
+  if (!token) return res.status(401).json({ error: TOKEN_INVALID })
 
   const notezUser = await prisma.user.findUnique({
     where: { email: `${token.email}` },
@@ -38,15 +44,15 @@ async function handleGET(req: NextApiRequest, res: NextApiResponse) {
   })
 
   if (!Number.isInteger(noteId))
-    return res.status(404).json({ error: "Please provide a valid ID." })
+    return res.status(404).json({ error: ID_INVALID })
 
   const note = await prisma.note.findUnique({
     where: { id: noteId },
   })
 
-  if (!note) return res.status(404).json({ error: "Note not found." })
+  if (!note) return res.status(404).json({ error: NOTE_NOT_FOUND })
   if (!notezUser?.notes.find((note) => noteId == note?.id)) {
-    return res.status(404).json({ error: "This isn't your note... :think:" })
+    return res.status(401).json({ error: NOTE_NOT_YOURS })
   }
 
   return res.status(200).json(note)
@@ -60,10 +66,7 @@ async function handlePATCH(req: NextApiRequest, res: NextApiResponse) {
   const secret = process.env.NEXTAUTH_SECRET
   const token = await getToken({ req, secret })
 
-  if (!token)
-    return res
-      .status(401)
-      .json({ error: "Please provide a valid token." })
+  if (!token) return res.status(401).json({ error: TOKEN_INVALID })
 
   const notezUser = await prisma.user.findUnique({
     where: { email: `${token.email}` },
@@ -74,9 +77,9 @@ async function handlePATCH(req: NextApiRequest, res: NextApiResponse) {
     where: { id: noteId },
   })
 
-  if (!note) return res.status(404).json({ error: "Note not found." })
+  if (!note) return res.status(404).json({ error: NOTE_NOT_FOUND })
   if (!notezUser?.notes.find((note) => noteId == note?.id)) {
-    return res.status(404).json({ error: "This isn't your note... :think:" })
+    return res.status(401).json({ error: NOTE_NOT_YOURS })
   }
 
   const newResult = await prisma.note.update({
@@ -89,4 +92,39 @@ async function handlePATCH(req: NextApiRequest, res: NextApiResponse) {
   })
 
   return res.status(200).json({ content: newResult?.content })
+}
+
+async function handleDELETE(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query
+  const noteId = Number(id)
+
+  const secret = process.env.NEXTAUTH_SECRET
+  const token = await getToken({ req, secret })
+
+  if (!token) return res.status(401).json({ error: TOKEN_INVALID })
+
+  const notezUser = await prisma.user.findUnique({
+    where: { email: `${token.email}` },
+    select: { id: true, notes: true },
+  })
+
+  if (!Number.isInteger(noteId))
+    return res.status(404).json({ error: ID_INVALID })
+
+  const note = await prisma.note.findUnique({
+    where: { id: noteId },
+  })
+
+  if (!note) return res.status(404).json({ error: NOTE_NOT_FOUND })
+  if (!notezUser?.notes.find((note) => noteId == note?.id)) {
+    return res.status(401).json({ error: NOTE_NOT_YOURS })
+  }
+
+  await prisma.note.delete({
+    where: {
+      id: noteId,
+    },
+  })
+
+  return res.status(200).json({ result: "Note deleted." })
 }
