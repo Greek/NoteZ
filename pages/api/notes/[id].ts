bbb353f13dde9ccad63bbb859899acf929e9f835
Andreas <a@yeat.dev>
2022-07-09 11:45:13 -0400

    feat: integrade edit endpoint into `notes/[id].ts`
    Notes can be edited using the PATCH http method.
    
    Signed-off-by: Andreas <a@yeat.dev>

--- pages/api/notes/[id].ts
+++ pages/api/notes/[id].ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import { getToken } from "next-auth/jwt"
+import { json } from "stream/consumers"
 
 import prisma from "../../../lib/prisma"
 
@@ -51,4 +52,41 @@ async function handleGET(req: NextApiRequest, res: NextApiResponse) {
   return res.status(200).json(note)
 }
 
-async function handlePATCH(req: NextApiRequest, res: NextApiResponse) {}
+async function handlePATCH(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query
+  const { content } = req.body
+  const noteId = Number(id)
+
+  const secret = process.env.NEXTAUTH_SECRET
+  const token = await getToken({ req, secret })
+
+  if (!token)
+    return res
+      .status(401)
+      .json({ error: "A token is required to make this request." })
+
+  const notezUser = await prisma.user.findUnique({
+    where: { email: `${token.email}` },
+    select: { id: true, notes: true },
+  })
+
+  const note = await prisma.note.findUnique({
+    where: { id: noteId },
+  })
+
+  if (!note) return res.status(404).json({ error: "Note not found." })
+  if (!notezUser?.notes.find((note) => noteId == note?.id)) {
+    return res.status(404).json({ error: "This isn't your note... :think:" })
+  }
+
+  const newResult = await prisma.note.update({
+    where: {
+      id: noteId,
+    },
+    data: {
+      content: content ?? note?.content,
+    },
+  })
+
+  return res.status(200).json({ content: newResult?.content })
+}
